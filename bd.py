393f829806ace967fcbe293d5356fb873eb559a3
DanielOlint0 <abrantesd2003@gmail.com>
2023-11-09 16:20:56 -0300

    Conexao banco de dados

---
+++ bd.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def _executar(query):
+    bd_path = './Biblioteca'
+    connection = sqlite3.connect(bd_path)
+    cursor = connection.cursor()
+    resultado = None
+    try:
+        cursor.execute(query)
+        resultado = cursor.fetchall()
+        connection.commit()
+    except Exception as e:
+        print(f'Erro na execuçao da query: {e}')
+    finally:
+        cursor.close()
+        connection.close()
