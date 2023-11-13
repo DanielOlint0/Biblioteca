ce4aa16a9e3c938697aa1cbe860791e7d1dcd705
Samuel Lucas <samuzica68@gmail.com>
2023-11-13 16:39:53 -0300

    Adicionado Teste do Controller Livro

---
+++ testeController.py
@@ -0,0 +1,19 @@
+
+from controller.livroController import AdicionarLivroController, AtualizarLivroController, ApagarLivroController, ListarLivrosController, BuscarLivroController
+
+if __name__ == '__main__':
+    #Adicionar
+    AdicionarLivroController.post("Livro Teste4", 456, "2023-01-04", 12, 1, 987, "Autor Teste4", "Editora Teste4")
+    AdicionarLivroController.post("Livro Teste4", 456, "2023-01-04", 12, 1, 987, "Autor Teste4", "Editora Teste4")
+    ListarLivrosController.get()
+    
+    #Excluir
+    ApagarLivroController.get(1)
+    ListarLivrosController.get()
+    
+    #Buscar
+    BuscarLivroController.get(2)
+    
+    #Atualizar
+    AtualizarLivroController.get(2, 0)
+    ListarLivrosController.get()
