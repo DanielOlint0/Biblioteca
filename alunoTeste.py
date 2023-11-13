d0225ad81b934955c59b244e05fee1c0ffdbb04e
Lavínia Ferreira <145711271+laviniaFer@users.noreply.github.com>
2023-11-13 16:11:03 -0300

    fizemo o teste de aluno (joinha)

---
+++ alunoTeste.py
@@ -0,0 +1,23 @@
+from model.alunoModel import Aluno
+from bd import _executar
+
+# if __name__ == '__main__':
+#     aluno1 = Aluno ("Flávio", 19, "DS")
+    # print (aluno1)
+    # aluno1.cadastrarAluno()
+    
+    # print (Aluno.listarAlunos())
+    # print (aluno1.listarAluno(3))
+    
+    # index = 2
+    # delete = Aluno.listarAluno(index)
+    
+    # excloi
+    # delete.excluirAluno()
+    # print (Aluno.listarAlunos())
+
+    #alterar
+    # Flavio3 = Aluno.listarAluno(3)
+    # Flavio3._nome = "Rafael"
+    # Flavio3.alterarAluno()
+    # print (Aluno.listarAlunos())
