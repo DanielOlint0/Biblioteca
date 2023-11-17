2b4f2ad5a93dd68476d2b61ceda2a43a7f106afd
DanielOlint0 <abrantesd2003@gmail.com>
2023-11-17 00:17:21 -0300

    Criado o controller do registro de emprestimo

---
+++ controller/registroDeEmprestimoController.py
@@ -0,0 +1,38 @@
+from model.registroDeEmprestimoModel import RegistroDeEmprestimo
+
+class Adicionar:
+    @staticmethod
+    def post(codigoUsuario, codigoMaterial, dataInicio, dataFinal):
+        registro = RegistroDeEmprestimo(codigoUsuario, codigoMaterial,dataInicio, dataFinal)
+        registro.salvar()
+
+class Atualizar:
+    @staticmethod
+    def get(id, status):
+        registro = RegistroDeEmprestimo.buscar_por_id(id)
+        registro.setStatus(status)
+        registro.alterar()
+
+class Apagar:
+    @staticmethod
+    def get(id):
+        registro = RegistroDeEmprestimo.buscar_por_id(id)
+        registro.excluir()
+
+class Listar:
+    @staticmethod
+    def get():
+        #print(RegistroDeEmprestimo.get_registros())
+        print(RegistroDeEmprestimo.get_registros())
+class BuscarRegistroPorID():
+    @staticmethod
+    def get(id):
+        registro = RegistroDeEmprestimo.buscar_por_id(id)
+        print(registro)
+
+class Busca():
+    @staticmethod
+    def get():
+        registros = RegistroDeEmprestimo.get_registros()
+        for registro in registros:
+            print(registro)
